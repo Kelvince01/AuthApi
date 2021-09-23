99733ba5bc82b5d9cb18af065aac9b56fc643e9f
kelvince pericles <kelvince05@outlook.com>
2021-09-23 16:08:35 +0300

    Added User Sample Test

--- AuthApiApp/tests.py
+++ AuthApiApp/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
 # Create your tests here.
+#from django.contrib.auth.models import User
+from .models import User
+
+
+# Create your tests here.
+
+class UserTestCase(TestCase):
+    def test_user(self):
+        email = 'sample@gmail.com'
+        username = 'sample'
+        password = 'sample@123'
+        u = User(email=email,username=username)
+        u.set_password(password)
+        u.save()
+        self.assertEqual(u.email, email)
+        self.assertTrue(u.check_password(password))
